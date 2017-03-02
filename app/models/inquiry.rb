eeb3d90f565989e03071a95d62c64e57a5a5e579
Raghu Betina <raghu@firstdraft.com>
2017-03-02 06:52:25 +0000

    Direct association between users and inquiries

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,6 +1,9 @@
 class Inquiry < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :inquiries,
+             :dependent => :destroy
+
   has_many   :whies,
              :dependent => :nullify
 
