cba776710c76635520d923543963908c183d7401
Raghu Betina <raghu@firstdraft.com>
2017-03-02 06:53:14 +0000

    Only post user can modify post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
+  before_action :current_user_must_be_post_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_post_user
+    post = Post.find(params[:id])
+
+    unless current_user == post.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @posts = Post.all
 
