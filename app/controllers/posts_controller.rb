f3399966be6ff86dea26e10e2a083180e6fab033
Raghu Betina <raghu@firstdraft.com>
2017-03-02 06:51:50 +0000

    Generate posts resource with starter:resource

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,81 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+
+    render("posts/index.html.erb")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+
+    render("posts/show.html.erb")
+  end
+
+  def new
+    @post = Post.new
+
+    render("posts/new.html.erb")
+  end
+
+  def create
+    @post = Post.new
+
+    @post.user_id = params[:user_id]
+    @post.body = params[:body]
+
+    save_status = @post.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/posts/new", "/create_post"
+        redirect_to("/posts")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Post created successfully.")
+      end
+    else
+      render("posts/new.html.erb")
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+
+    render("posts/edit.html.erb")
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    @post.user_id = params[:user_id]
+    @post.body = params[:body]
+
+    save_status = @post.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/posts/#{@post.id}/edit", "/update_post"
+        redirect_to("/posts/#{@post.id}", :notice => "Post updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Post updated successfully.")
+      end
+    else
+      render("posts/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    if URI(request.referer).path == "/posts/#{@post.id}"
+      redirect_to("/", :notice => "Post deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Post deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Post resource:
+  # CREATE
+  get "/posts/new", :controller => "posts", :action => "new"
+  post "/create_post", :controller => "posts", :action => "create"
+
+  # READ
+  get "/posts", :controller => "posts", :action => "index"
+  get "/posts/:id", :controller => "posts", :action => "show"
+
+  # UPDATE
+  get "/posts/:id/edit", :controller => "posts", :action => "edit"
+  post "/update_post/:id", :controller => "posts", :action => "update"
+
+  # DELETE
+  get "/delete_post/:id", :controller => "posts", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
