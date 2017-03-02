5d9e78f0e186e2491d4238615de4385fd63006bf
Raghu Betina <raghu@firstdraft.com>
2017-03-02 06:52:19 +0000

    Direct association between users and vosts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :vosts,
+             :dependent => :nullify
+
   has_many   :questions,
              :class_name => "Post",
              :dependent => :nullify
